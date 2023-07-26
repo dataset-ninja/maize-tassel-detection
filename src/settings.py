b490edff90d5dba867039685a0b452250c80e362
vorozhkog <vorozkhog@icloud.com>
2023-07-26 13:25:38 +0100

    updated

---
+++ src/settings.py
@@ -0,0 +1,109 @@
+from typing import Dict, List, Optional, Union
+
+from dataset_tools.templates import (
+    AnnotationType,
+    CVTask,
+    Industry,
+    Domain,
+    Research,
+    License,
+    Category,
+)
+
+##################################
+# * Before uploading to instance #
+##################################
+PROJECT_NAME: str = "Maize tassel detection"
+PROJECT_NAME_FULL: str = "Maize tassel detection from UAV imagery using deep learning"
+
+##################################
+# * After uploading to instance ##
+##################################
+LICENSE: License = License.CC0_1_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [
+    Industry.Agricultural(),
+    Domain.Industrial(),
+    Research.Agricultural(),
+]
+CATEGORY: Category = Category.Agriculture()
+
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
+
+RELEASE_DATE: Optional[str] = "2021-06-09"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
+HOMEPAGE_URL: str = "https://zenodo.org/record/4922074#.Yk_rlX9Bzmg"
+# e.g. "https://some.com/dataset/homepage"
+
+PREVIEW_IMAGE_ID: int = 348960
+# This should be filled AFTER uploading images to instance, just ID of any image.
+
+GITHUB_URL: str = "https://github.com/dataset-ninja/maize-tassel-detection"
+# URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
+
+##################################
+### * Optional after uploading ###
+##################################
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+# Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
+
+CLASS2COLOR: Optional[Dict[str, List[str]]] = None
+# If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
+
+PAPER: Optional[str] = "https://doi.org/10.5061/dryad.r2280gbcg"
+CITATION_URL: Optional[str] = None
+AUTHORS: Optional[List[str]] = None
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
+TAGS: Optional[List[str]] = None
+
+##################################
+###### ? Checks. Do not edit #####
+##################################
+
+
+def check_names():
+    fields_before_upload = [PROJECT_NAME]  # PROJECT_NAME_FULL
+    if any([field is None for field in fields_before_upload]):
+        raise ValueError("Please fill all fields in settings.py before uploading to instance.")
+
+
+def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
+    settings = {
+        "project_name": PROJECT_NAME,
+        "license": LICENSE,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
+        "cv_tasks": CV_TASKS,
+        "annotation_types": ANNOTATION_TYPES,
+        "release_year": RELEASE_YEAR,
+        "homepage_url": HOMEPAGE_URL,
+        "preview_image_id": PREVIEW_IMAGE_ID,
+        "github_url": GITHUB_URL,
+    }
+
+    if any([field is None for field in settings.values()]):
+        raise ValueError("Please fill all fields in settings.py after uploading to instance.")
+
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
+    settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
+    settings["class2color"] = CLASS2COLOR
+    settings["paper"] = PAPER
+    settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
+    settings["organization_name"] = ORGANIZATION_NAME
+    settings["organization_url"] = ORGANIZATION_URL
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
+
+    return settings
